621cc7efee45e3821f0f98142e0618203e3ab3c8
Tatyana Soloveva <tanuysya@mail.ru>
2023-11-26 14:53:09 +0300

    Создан репозиторий

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿///Задача 2: Задайте массив на 10 целых чисел. 
+///Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int size = 10;
+int[] array = new int[size];
+Random r = new Random();
+for (int i = 0; i < size; i++)
+    array[i] = r.Next(10);
+
+for (int i = 0; i < size; i++)
+    Console.Write(array[i] + " ");
+
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i] % 2 == 0)
+        count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine(count);
